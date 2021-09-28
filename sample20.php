dda30d32931be37248447d1d4d41b362c014264d
planetes230 <planet.planet230@gmail.com>
2021-09-29 02:19:29 +0900

    file_upload

---
+++ sample20.php
@@ -0,0 +1,19 @@
+<?php
+$file = $_FILES['my_img'];
+print('ファイル名(name):'.$file['name'].'<br />');
+print('ファイルタイプ(type):'.$file['type'].'<br />');
+print('アップロードしたファイル(tmp_name):'.$file['tmp_name'].'<br />');
+print('エラー内容(error):'.$file['error'].'<br />');
+print('サイズ(size):'.$file['size'].'<br />');
+
+//ファイルアップロードのsy9降りをする
+$ext = substr($file['name'],-4);
+if($ext == '.gif' || $ext == '.jpg' || $ext == '.png') {
+  $filePath = './user_img/'.$file['name'];
+  move_uploaded_file($file['tmp_name'],$filePath);
+  print('<img src="'.$filePath.'" />');
+} else {
+  print('※拡張子が.gif,.jpg,.pngのいずれかのファイルをアップロードしてください');
+}
+
+?>
